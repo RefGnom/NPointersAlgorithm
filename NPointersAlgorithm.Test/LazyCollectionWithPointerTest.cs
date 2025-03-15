ece63701f868dbcc4ec5e60846ef0f503b6b7291
Cyrus <88629892+RefGnom@users.noreply.github.com>
2025-03-16 00:30:02 +0500

    Начал обмазывать тестами

---
+++ NPointersAlgorithm.Test/LazyCollectionWithPointerTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FluentAssertions;
+
+namespace NPointersAlgorithm.Test;
+
+public class LazyCollectionWithPointerTest : TestBase
+{
+    [Test]
+    public void TestCreateWithEmptyCollection_ShouldBeEmpty()
+    {
+        var lazyCollectionWithPointer = CreateLazyCollectionWithPointer(Array.Empty<long>());
+
+        lazyCollectionWithPointer.IsEmpty.Should().BeTrue();
+    }
+
+    [Test]
+    public void TestCreateWithEmptyCollection_CurrentPointerShouldThrowsException()
+    {
+        var lazyCollectionWithPointer = CreateLazyCollectionWithPointer(Array.Empty<long>());
+
+        Assert.Throws<NPointersAlgorithmException>(() => _ = lazyCollectionWithPointer.CurrentPointer);
+    }
+
+    [Test]
+    public void TestCreateWithEmptyCollection_GetItemShouldThrowsException()
+    {
+        var lazyCollectionWithPointer = CreateLazyCollectionWithPointer(Array.Empty<long>());
+
+        Assert.Throws<NPointersAlgorithmException>(() => lazyCollectionWithPointer.GetItem());
+    }
+}
